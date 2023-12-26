beba1ba6f677e6b80dd342a0430ab068df9a02c0
prajin <prajin.p@ultsglobal.com>
2023-12-26 11:40:04 +0530

    fix: ipforwording

--- install.py
+++ install.py
@@ -446,7 +446,13 @@ br_netfilter
 EOF""")
     execute_command("sudo modprobe -a overlay br_netfilter")
     execute_command("sudo modprobe br_netfilter")
-
+    execute_command("""cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF""")
+    
+    execute_command("sudo sysctl --system")
     # If not installed, follow the provided steps to install containerd
 
 
